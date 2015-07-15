43090af08a7602ada49e51b76d507e4cb9602938
Eugene Choi <eugene.maestro.choi@gmail.com>
2015-07-16 07:59:52 +0800

    improve history logice on grade 5 - 8

--- public/js/app/grade5/controller.js
+++ public/js/app/grade5/controller.js
@@ -1,7 +1,6 @@
 'use strict';
 App.Grade5.controller = {
   init: function() {
-
   },
   initTabs: function() {
     var hash = location.hash;
@@ -9,7 +8,7 @@ App.Grade5.controller = {
       setTimeout(function() {
         window.scrollTo(0, 0);
       }, 1);
-      
+
       var $myTab = $(hash);
       if ($myTab.size() != 0) {
         $('a[href=#' + $myTab.attr('id') + ']').click();
@@ -22,7 +21,12 @@ App.Grade5.controller = {
     }
 
     $('a[role="tab"]').click(function() {
-      window.location.hash = ($(this).attr('href')).substr(1);
+      var hash = ($(this).attr('href')).substr(1);
+      if (history && history.replaceState) {
+        history.replaceState(undefined, undefined, '#' + hash);
+      } else {
+        location.replace('#' + hash);
+      }
     });
   }
 };
