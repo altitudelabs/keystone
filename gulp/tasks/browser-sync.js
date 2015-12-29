8ce8d823cc02ead7c38a78494d905968b611f180
Eugene Choi <eugene.maestro.choi@gmail.com>
2015-12-29 13:12:49 +0800

    fix port number for browser sync

--- gulp/tasks/browser-sync.js
+++ gulp/tasks/browser-sync.js
@@ -9,7 +9,7 @@ var browserSync = require('browser-sync');
 module.exports = function () {
   return gulp.task('browser-sync', function(){
     browserSync({
-      proxy: 'localhost:3000',  // local node app address
+      proxy: 'localhost:8888',  // local node app address
       port: 5000,  // use *different* port than above
       notify: true
     });
