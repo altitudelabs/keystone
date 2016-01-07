89a64c4ac587691e749caee8a260fe165c43cb80
ayame30 <alnaym30@gmail.com>
2016-01-07 18:00:30 +0800

    hook up snapshot

--- routes/views/snapshot.js
+++ routes/views/snapshot.js
@@ -1,15 +1,24 @@
 var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
-  
+
   var view = new keystone.View(req, res),
     locals = res.locals;
-  
+
   // locals.section is used to set the currently selected
   // item in the header navigation.
   locals.section = 'snapshot';
-  
+  view.on('init', function(next) {
+
+    var q = keystone.list('Service').model.findOne({
+      'slug':'bottom-section-2'
+    });
+    q.exec(function(err, result) {
+      locals.data = result;
+      next(err);
+    });
+  });
   // Render the view
   view.render('snapshot');
-  
+
 };
